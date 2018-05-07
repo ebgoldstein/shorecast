cd73c71fb26115324ea1a3ba5cf8f579b97932fc
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2018-05-07 09:56:44 -0400

    Update ShorecastEBG.R

--- ShorecastEBG.R
+++ ShorecastEBG.R
@@ -1,4 +1,5 @@
 #https://keras.rstudio.com
 #https://tensorflow.rstudio.com/keras/
+#https://tensorflow.rstudio.com/blog/time-series-forecasting-with-recurrent-neural-networks.html
 
 library(keras)
