7556149bc43f09a0bcec22301ba7919fddd591e0
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2018-05-09 10:23:45 -0400

    Update ShorecastEBG.R

--- ShorecastEBG.R
+++ ShorecastEBG.R
@@ -1,5 +1,28 @@
+#Use LSTM to make shoreline change predictions
+#local file structure is "Case1" and "Case 2" folders are within a folder entitled "Data"
+#
+#EBG 5/2018
+#MIT license
+#
+#Some helpful info to keep handy:
 #https://keras.rstudio.com
 #https://tensorflow.rstudio.com/keras/
-#https://tensorflow.rstudio.com/blog/time-series-forecasting-with-recurrent-neural-networks.html
 
 library(keras)
+library(tidyverse)
+
+#CASE 1
+
+#load the wave data
+waves <- read_delim("Data/Case1/Waves/Wave hindcast_corrected.txt", delim = " ", local = locale(encoding = "latin1"))
+
+#load the tide data
+tides <- read_delim("Data/Case1/Tides/tide past.txt", delim = " ", local = locale(encoding = "latin1"))
+
+#load shorelines
+shoreline <- read_delim("Data/Case1/Shorelines/Camera/Averaged shoreline data.txt", delim = " ", local = locale(encoding = "latin1"))
+
+#load SL
+
+#join
+#recover plots
